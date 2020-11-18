5233170e27034c92a40ff345697cadfe6c4cb831
goteninwza007 <73677460+goteninwza007@users.noreply.github.com>
2020-11-18 22:15:41 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,21 +1,16 @@
 <?php
 
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'chalaheadchala.mysql.database.azure.com', 'goten@chalaheadchala', 'Ggggg12188', 'ITFlab', 3306);
+$conn = mysqli_connect($conn, 'chalaheadchala.mysql.database.azure.com', 'goten@chalaheadchala', 'Ggggg12188', 'ITFlab', 3306);
+$id = $_GET['ID'];
+$sql = "DELETE FROM guestbook WHERE ID = '$id'";
+
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
-
-$id = $_GET['ID'];
-
-
-$sql = "DELETE FROM guestbook WHERE ID = '$id'";
-
-
 if (mysqli_query($conn, $sql)) {
-    echo "Successfully";
+    echo "Successfully.";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
