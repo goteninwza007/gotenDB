3504512fb051aac64606e3d8a87e97e3912e1659
goteninwza007 <73677460+goteninwza007@users.noreply.github.com>
2020-11-18 09:41:03 +0700

    Update show.php

--- show.php
+++ show.php
@@ -15,9 +15,9 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 <table width="600" border="1">
   <tr>
     <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-    <th width="200"> <div align="center">Delete </div></th>
+    <th width="350"> <div align="center">Comment</div></th>
+    <th width="150"> <div align="center">Link</div></th>
+    <th width="200"> <div align="center">Function</div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
