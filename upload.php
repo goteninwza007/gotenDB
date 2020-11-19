1ca622837646c80ac161e0a0a91bc9477149e5a7
goteninwza007 <73677460+goteninwza007@users.noreply.github.com>
2020-11-19 15:13:46 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,13 +2,13 @@
 
 $conn = mysqli_connect('chalaheadchala.mysql.database.azure.com', 'goten@chalaheadchala', 'Ggggg12188', 'ITFlab', 3306);
 
-
+$id = $_POST['ID'];
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];
 
 
-$sql = 'UPDATE guestbook SET name="'.$name.'",comment="'.$comment.'",link="'.$link.'';
+$sql = 'UPDATE guestbook SET name="'.$name.'",comment="'.$comment.'",link="'.$link.'" WHERE ID='.$id.'';
 
 
 if (mysqli_query($conn, $sql)) {
